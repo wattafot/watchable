5d69154b94ffac74c33eb4181792cece8ead7aab
Frieder Vibrans <friedervibrans@MacBook-Air-von-Frieder.local>
2024-01-02 13:15:34 +0100

    carouselbanner done

--- components/CarouselBanner.tsx
+++ components/CarouselBanner.tsx
@@ -27,16 +27,17 @@ function CarouselBanner({ movies }: { movies: Movie[] }) {
               width={1920}
               height={1080}
             />
-            <div className="hidden md:inline absolute top-0 left-0 z-20 p-12 bg-transparent w-full h-full bg-gradient-to-r from-gray-900/90 via-transparent to-transparent">
-              <h2 className="text-5xl font-bold max-w-xl z-50 py-4">
+            <div className="hidden md:inline absolute top-0 left-0 p-12 bg-transparent w-full h-full bg-gradient-to-r from-gray-900/90 via-transparent to-transparent">
+              <div className="h-[65%]" />
+              <h2 className="text-3xl font-bold max-w-xl py-4">
                 {movie.title}
               </h2>
-              <p className="max-w-xl line-clamp-3">{movie.overview}</p>
+              <p className=" max-w-lg line-clamp-3">{movie.overview}</p>
             </div>
           </div>
         ))}
       </div>
-      <div className="absolute inset-0 bg-gradient-to-b from-gray-200/0 via-gray-200/20 to-gray-900" />
+      <div className="absolute inset-0 bg-gradient-to-b from-gray-900/0 to-gray-900" />
     </div>
   );
 }
